48a1490e1e5f9bfc902fb8188d982efa927158bb
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-01-19 11:44:53 -0500

    beat fetch

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Beats = () => {
-  return <div>Beats</div>;
+const Beats = async () => {
+  const data = await fetch("http://localhost:7007/beat");
+  const beats = await data.json();
+  return (
+    <ul>
+      {beats.map((beat: any) => (
+        <li key={beat.id}>{beat.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Beats;
