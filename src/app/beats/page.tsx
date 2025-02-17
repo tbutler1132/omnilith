f59b7c2fdf7b54401b2f77f5ea280eedf64f64af
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-02-17 14:11:50 -0500

    beat stuff

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 
 const Beats = async () => {
-  const data = await fetch("http://localhost:7007/beat");
-  const beats = await data.json();
-  return (
-    <ul>
-      {beats.map((beat: any) => (
-        <li key={beat.id}>{beat.title}</li>
-      ))}
-    </ul>
+  const client = createStorefrontApiClient({
+    storeDomain: "omnilithbeats.myshopify.com",
+    apiVersion: "2023-10",
+    publicAccessToken: "c7ac853b758015d9a0ce36b597726537",
+  });
+
+  const data = await client.request(
+    `query getProducts($first: Int) {
+      products(first: $first) {
+        edges {
+          cursor
+          node {
+            title
+          }
+        }
+      }
+    }`,
+    {
+      variables: {
+        first: 1,
+      },
+    }
   );
+
+  console.log("Client", data.data.products.edges[0].node);
+
+  return <ul>ear</ul>;
 };
 
 export default Beats;
