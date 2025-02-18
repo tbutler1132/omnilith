b7bc4976359f0e0672f1cdd0e2474e595c7b2f9b
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-02-18 17:56:57 -0500

    embedded airbit and added artifacts directory

--- src/app/beats/page.tsx
+++ src/app/beats/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import styles from "./beats.module.css";
 
 const Beats = () => {
-  return <div>Beats</div>;
+  return (
+    <div style={{ height: "100vh" }}>
+      <h1 className={styles.glowingText}>Beats</h1>
+      <iframe
+        id="airbit_infinity"
+        src="https://omnilith.infinity.airbit.com?config_id=16353&embed=1"
+        width="100%"
+        height="90%"
+        frame-border="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Beats;
